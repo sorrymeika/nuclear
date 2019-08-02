0d8417e93d8e6e99dd3a35725328baf159dc4f84
孙路 <>
2019-08-02 11:31:22 +0800

    add template

--- src/nuclear.js
+++ src/nuclear.js
@@ -1,2 +1,12 @@
+import * as antd from 'antd';
+import * as moment from 'moment';
+import { util, $ } from 'snowball';
+
 export * from "./atom";
 export * from "./components";
+
+$(window).on('load resize', () => {
+    util.style('antd-hack', `.ant-modal-body { max-height: ${window.innerHeight - 200}px; overflow-y:auto; }`, true);
+});
+
+export { antd, moment };
